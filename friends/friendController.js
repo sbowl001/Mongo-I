2eb14d292a726f646958e00f571f8cfc127ccc28
Stephanie Bowles <sbowl001@fiu.edu>
2018-05-31 15:30:21 +0900

    put and delete from lecture, gotta model to assignments reqs

--- friends/friendController.js
+++ friends/friendController.js
@@ -27,4 +27,36 @@ router
             })
     });
 
+    router
+        .route('/:id')
+        .get((req, res) => {
+            const {id} = req.params;
+            Friend.findById(id)
+            res.status(200).json({route: '/api/friends/' + req.params.id });
+        })
+        .delete((req, res) => {
+            const {id} = req.params;
+            Friend.findByIdAndRemove(id)
+                .then( friend => {
+                    res.status(200).json(friend);
+                })
+                .catch( err => res.status(500).json(err));
+        })
+        .put((req, res) => {
+            const {id} = req.params;
+            const friendData = req.body;
+            const options = {
+                new: true
+            }
+            Friend.findByIdAndUpdate(id, friendData, options)
+            .then( friend => {
+                if( friend !== null) {
+                    res.status(200).json(friend);
+                } else {
+                    res.sendStatus(404);
+                }
+            })
+            .catch(err => res.status(500).json(err))
+        });
+
     module.exports = router;
