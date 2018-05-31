892f56235cb811e4d7528c05997b9bd0bd4305a1
Stephanie Bowles <sbowl001@fiu.edu>
2018-05-31 15:15:46 +0900

    basic setup, get&post

---
+++ friends/friendModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose'); 
+
+const definition = { 
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now,
+    },
+};
+
+const options = {
+    timestamps: true,
+};
+
+const friendSchema = new mongoose.Schema(definition, options);
+
+const friendModel = mongoose.model('Friend', friendSchema, 'friends');
+
+module.exports = friendModel;
+
+// {
+//     firstName: "Jane", // String, required
+//     lastName: "Doe",  // String, required
+//     age: 18, // Number, required, should be an integer between 1 and 120
+//     createdOn: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, required, defaults to current date
+//   }
--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+
+const mongoose = require('mongoose');
+
+mongoose 
+  .connect('mongodb://localhost/friendsdb')  //did I have to write friendsdb by myself?
+  .then(mongo => {
+    console.log('connected to database');
+  })
+  .catch( err => {
+    console.log('error connecting to the database', err)
+  });
+
+const friendController = require('./friends/friendController');
+
 const server = express();
 
 server.use(helmet());
@@ -11,6 +25,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
